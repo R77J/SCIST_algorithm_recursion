41033681100ccd523a36f04807c282c4209c5bf5
R77J <jerry970131@yahoo.com>
2024-12-11 10:03:26 +0800

    uva839 modified

--- uva839.cpp
+++ uva839.cpp
@@ -1,5 +1,46 @@
 #include <bits/stdc++.h>
-bool isbalance(long long int& totalweight)
+bool isbalance=true;
+long long int balance()
+{
+    long long int w1;
+    long long int l1;
+    long long int w2;
+    long long int l2;
+    std::cin>>w1>>l1>>w2>>l2;
+    if(l1==0||l2==0) return w1+w2;
+    if(w1==0)
+    {
+        w1=balance();
+    }
+    if(w2==0)
+    {
+        w2=balance();  
+    }
+    if(w1*l1!=w2*l2)isbalance=false;
+    
+    return w1+w2;
+}
+int main()
+{
+    long long int n=0;
+    std::cin>>n;
+    for(long long int i=0;i<n;i++)
+    {
+        isbalance=true;
+        balance();
+        if(!isbalance)
+        {
+            std::cout<<"NO\n";
+            continue;
+        }
+        std::cout<<"YES\n";
+    }
+
+    return 0;
+}
+/*AI 建議version
+
+bool isbalance(long long int& totalweight)//ai建議的用bool function(但一樣WA)
 {   
     long long int w1;
     long long int l1;
@@ -20,25 +61,9 @@ bool isbalance(long long int& totalweight)
     else return false;
 
 }
-int main()
-{
-    long long int n=0;
-    std::cin>>n;
-    long long int totalweight=0;
-    for(long long int i=0;i<n;i++)
-    {
-        totalweight=0;
-        if(!isbalance(totalweight))
-        {
-            std::cout<<"NO\n";
-            continue;
-        }
-        std::cout<<"YES\n";
-    }
 
-    return 0;
-}
 
+*/
 /*原判斷函式
 
 bool isbalance=true;
